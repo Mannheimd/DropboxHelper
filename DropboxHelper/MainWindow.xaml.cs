0f2f69f458c1eed60dc4ae15a81abe45df4f7a09
Chris Manders <cmanders159@gmail.com>
2018-03-12 08:54:16 +0000

    Add encryption and registry storage for Dropbox Access Token

--- DropboxHelper/MainWindow.xaml.cs
+++ DropboxHelper/MainWindow.xaml.cs
@@ -16,11 +16,13 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Net.Http;
+using System.Security.Cryptography;
 using Dropbox.Api;
 using Dropbox.Api.Auth;
 using Dropbox.Api.Files;
 using Dropbox.Api.Users;
 using Dropbox.Api.Sharing;
+using Microsoft.Win32;
 
 namespace DropboxHelper
 {
@@ -425,6 +427,139 @@ namespace DropboxHelper
             {
                 //TODO: Add error handling
                 return null;
+            }
+        }
+    }
+
+    public class DropboxAuth
+    {
+        public static byte[] additionalEntropy = { 7, 2, 6, 5 ,9 }; // Used to further encrypt authentication information, changing this will cause any currently stored login details on the client machine to be invalid
+
+        /// <summary>
+        /// Secures the user's credentials against the Windows user profile and stores them in the registry under HKCU
+        /// </summary>
+        /// <param name="apiToken">API token to be stored</param>
+        /// <param name="id">Name for the key</param>
+        public static void SecureCreds(string accessToken, string id)
+        {
+            byte[] utf8Creds = UTF8Encoding.UTF8.GetBytes(accessToken);
+
+            byte[] securedCreds = null;
+
+            // Encrypt credentials
+            try
+            {
+                securedCreds = ProtectedData.Protect(utf8Creds, additionalEntropy, DataProtectionScope.CurrentUser);
+
+                // Check if registry path exists
+                if (CheckOrCreateRegPath())
+                {
+                    // Save encrypted key to registry
+                    RegistryKey credsKey = Registry.CurrentUser.OpenSubKey(@"Software\Swiftpage Support\Dropbox Logins", true);
+                    credsKey.SetValue(id, securedCreds);
+                }
+            }
+            catch (CryptographicException error)
+            {
+                //TODO: MessageHandler.handleMessage(false, 3, error, "Encrypting Jenkins login credentials");
+            }
+        }
+
+        /// <summary>
+        /// Pulls stored credentials from registry and decrypts them
+        /// </summary>
+        /// <param name="id">Name for the key</param>
+        /// <returns>Returns unsecured utf8-encrypted byte array representing stored credentials</returns>
+        public static byte[] UnsecureCreds(string id)
+        {
+            // Check if registry path exists
+            if (CheckOrCreateRegPath())
+            {
+                byte[] securedCreds = null;
+                byte[] utf8Creds = null;
+
+                // Get encrypted key from registry
+                try
+                {
+                    RegistryKey credsKey = Registry.CurrentUser.OpenSubKey(@"Software\Swiftpage Support\Dropbox Logins", false);
+                    securedCreds = (byte[])credsKey.GetValue(id);
+
+                    // Un-encrypt credentials
+                    try
+                    {
+                        utf8Creds = ProtectedData.Unprotect(securedCreds, additionalEntropy, DataProtectionScope.CurrentUser);
+                    }
+                    catch (CryptographicException error)
+                    {
+                        //TODO: MessageHandler.handleMessage(false, 3, error, "Decrypting stored Jenkins login credentials"); ;
+                    }
+                }
+                catch (Exception error)
+                {
+                    //TODO: MessageHandler.handleMessage(false, 3, error, "Locating reg key to get Jenkins credentials");
+                }
+
+                return utf8Creds;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Verifies that the registry key to store credentials exists, and creates it if not
+        /// </summary>
+        /// <returns>true if key is now created and valid, false if not</returns>
+        public static bool CheckOrCreateRegPath()
+        {
+            //TODO: MessageHandler.handleMessage(false, 6, null, "Verifying Jenkins Login registry key path");
+            RegistryKey key = null;
+
+            // Check if subkey "HKCU\Software\Swiftpage Support" exists
+            key = Registry.CurrentUser.OpenSubKey(@"Software\Swiftpage Support", false);
+            if (key == null)
+            {
+                //TODO: MessageHandler.handleMessage(false, 5, null, @"Creating registry key 'HKCU\Software\Swiftpage Support'");
+
+                try
+                {
+                    key = Registry.CurrentUser.OpenSubKey(@"Software", true);
+                    key.CreateSubKey("Swiftpage Support");
+                }
+                catch (Exception error)
+                {
+                    //TODO: MessageHandler.handleMessage(false, 3, error, @"Attempting to create registry key 'HKCU\Software\Swiftpage Support'");
+                    return false;
+                }
+            }
+
+            // Check if subkey HKCU\Software\Swiftpage Support\Dropbox Logins exists
+            key = Registry.CurrentUser.OpenSubKey(@"Software\Swiftpage Support\Dropbox Logins", false);
+            if (key == null)
+            {
+                //TODO: MessageHandler.handleMessage(false, 5, null, @"Creating registry key 'HKCU\Software\Swiftpage Support\Dropbox Logins'");
+
+                try
+                {
+                    key = Registry.CurrentUser.OpenSubKey(@"Software\Swiftpage Support", true);
+                    key.CreateSubKey("Dropbox Logins");
+                }
+                catch (Exception error)
+                {
+                    //TODO: MessageHandler.handleMessage(false, 3, error, @"Attempting to create registry key 'HKCU\Software\Swiftpage Support\Dropbox Logins'");
+                    return false;
+                }
+            }
+
+            // Confirm that full subkey exists
+            key = Registry.CurrentUser.OpenSubKey(@"Software\Swiftpage Support\Dropbox Logins", false);
+            if (key != null)
+            {
+                //TODO: MessageHandler.handleMessage(false, 6, null, "Login reg key exists");
+                return true;
+            }
+            else
+            {
+                //TODO: MessageHandler.handleMessage(false, 3, null, @"Testing access to key HKCU\Swiftpage Support\Dropbox Logins");
+                return false;
             }
         }
     }
