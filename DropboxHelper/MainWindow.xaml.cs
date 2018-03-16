be4576ebdfe6342dd9c0c57507b627b574829286
Chris Manders <cmanders159@gmail.com>
2018-03-16 18:22:32 +0000

    Removed unnecessary usings

--- DropboxHelper/MainWindow.xaml.cs
+++ DropboxHelper/MainWindow.xaml.cs
@@ -1,27 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Web;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Net.Http;
 using System.Security.Cryptography;
 using Dropbox.Api;
-using Dropbox.Api.Auth;
 using Dropbox.Api.FileRequests;
 using Dropbox.Api.Files;
-using Dropbox.Api.Users;
 using Dropbox.Api.Sharing;
 using Microsoft.Win32;
 
